4c1ee8f215b35a90bb621e9baa8b3c73a59e2af4
Eidzoku <r.obramovitch@gmail.com>
2022-09-20 08:45:30 +0300

    Asteroid fix
    Fixed bug when instansiated asteroids were spawned with disabled components

--- Assets/Scripts/Environment/Interactive/SpaceObjects/AsteroidShattering.cs
+++ Assets/Scripts/Environment/Interactive/SpaceObjects/AsteroidShattering.cs
@@ -16,14 +16,18 @@ namespace SpaceShooter
         [SerializeField] private Size size;
         [SerializeField] private float _randomSpeed;
 
+        [SerializeField] private AsteroidShattering _asteroidPrefab;
+
         #endregion
 
         #region Unity Events
-
         private void Awake()
         {
-            _eventOnDeath.AddListener(OnAsteroidDestroyed);
             SetSize(size);
+
+            Invoke(nameof(EnableCollider), 1);
+
+            _eventOnDeath.AddListener(OnAsteroidDestroyed);
         }
 
         private void OnDestroy()
@@ -42,26 +46,26 @@ namespace SpaceShooter
                 return;
             }
 
-            SpawnStones();
+            for (int i = 0; i < 2; i++)
+            {
+                SpawnStones();
+            }
+
             Destroy(gameObject);
         }
 
         private void SpawnStones()
         {
-            for (int i = 0; i < 2; i++)
+            AsteroidShattering asteroid = Instantiate(_asteroidPrefab, transform.position, Quaternion.identity);
+            asteroid.SetSize(size - 1);
+
+            asteroid._hitPoints = Mathf.Clamp(_hitPoints / 2, 1, _hitPoints);
+
+            Rigidbody2D rigidbody = asteroid.GetComponent<Rigidbody2D>();
+
+            if (rigidbody != null && _randomSpeed > 0)
             {
-                AsteroidShattering asteroid = Instantiate(this, transform.position, Quaternion.identity);
-                asteroid.SetSize(size - 1);
-                asteroid.GetComponent<AsteroidShattering>().EventOnDeath.AddListener(OnAsteroidDestroyed);
-
-                asteroid._hitPoints = Mathf.Clamp(_hitPoints / 2, 1, _hitPoints);
-
-                Rigidbody2D rigidbody = asteroid.GetComponent<Rigidbody2D>();
-
-                if (rigidbody != null && _randomSpeed > 0)
-                {
-                    rigidbody.velocity = (Vector2)Random.insideUnitSphere * _randomSpeed;
-                }
+                rigidbody.velocity = (Vector2)Random.insideUnitSphere * _randomSpeed;
             }
         }
 
@@ -80,6 +84,11 @@ namespace SpaceShooter
             if (size == Size.Small) return new Vector3(0.6f, 0.6f, 0.6f);
 
             return Vector3.one;
+        }
+
+        private void EnableCollider()
+        {
+            gameObject.GetComponentInChildren<CircleCollider2D>().enabled = true;
         }
         #endregion
     }
